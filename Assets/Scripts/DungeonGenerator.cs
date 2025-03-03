0910cde54ae57dcf7a21cf4d7393dc218d521443
Juliette Landman <Juliettelandman@gmail.com>
2025-03-03 17:27:43 +0100

    Added Doors

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DungeonGenerator : MonoBehaviour
 {
     public RectInt room;
-    public Vector2 minRoomSizeRage = new Vector2(10,20);
-    
+    public Vector2 minRoomSizeRage = new Vector2(10, 20);
+
     Coroutine drawCoroutine;
     List<RectInt> roomsToDraw = new List<RectInt>();
     List<RectInt> roomsToSplit = new List<RectInt>();
+    List<RectInt> Doors = new List<RectInt>();
+    List<RectInt> DoorIntersections = new List<RectInt>();
 
     void Start()
     {
@@ -19,10 +22,10 @@ public class DungeonGenerator : MonoBehaviour
 
     void Update()
     {
-        AddRooms();
+        Drawing();
     }
 
-    void AddRooms()
+    void Drawing()
     {
         AlgorithmsUtils.DebugRectInt(room, Color.red);
 
@@ -35,10 +38,15 @@ public class DungeonGenerator : MonoBehaviour
         {
             AlgorithmsUtils.DebugRectInt(roomsToDraw[i], Color.green);
         }
-    }
 
+        for (int i = 0; i < Doors.Count; i++)
+        {
+            AlgorithmsUtils.DebugRectInt(Doors[i], Color.cyan);
+        }
+    }
     IEnumerator DrawCoroutine()
     {
+        //Rooms
         yield return new WaitForEndOfFrame();
         Debug.Log("coroutine start");
 
@@ -49,37 +57,78 @@ public class DungeonGenerator : MonoBehaviour
 
             int minRoomSizeRange = Random.Range((int)minRoomSizeRage.x, (int)minRoomSizeRage.y);
 
-            if (currentRoom.width < minRoomSizeRange * 2&& currentRoom.height < minRoomSizeRange * 2)
+            if (currentRoom.width < minRoomSizeRange * 2 && currentRoom.height < minRoomSizeRange * 2)
             {
                 roomsToDraw.Add(currentRoom);
+
             }
             else SplitRooms(currentRoom);
 
             yield return new WaitForSeconds(0.1f);
         }
-        Debug.Log("drawing is done");
-    }
 
+        //Doors
+        for (int i = 0; i < roomsToDraw.Count; i++)
+        {
+            for (int j = i + 1; j < roomsToDraw.Count; j++)
+            {
+                if (AlgorithmsUtils.Intersects(roomsToDraw[i], roomsToDraw[j]))
+                {
+                    RectInt Inter = AlgorithmsUtils.Intersect(roomsToDraw[i], roomsToDraw[j]);
+                    if ((Inter.width == 1 && Inter.height > 5) || (Inter.height == 1 && Inter.width > 5))
+                    {
+                        DoorIntersections.Add(Inter);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < DoorIntersections.Count; i++)
+        {
+            MakeDoor(DoorIntersections[i]);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        Debug.Log("drawing is done; Total room count: " + roomsToDraw.Count + "|  total Intersections: " + DoorIntersections.Count);
+
+    }
     void SplitRooms(RectInt pRoom)
     {
         RectInt room1, room2; int splitRandom;
 
-        if (pRoom.width>= pRoom.height)//vertical
+        if (pRoom.width >= pRoom.height)//vertical
         {
             splitRandom = Random.Range((int)minRoomSizeRage.x, pRoom.width - (int)minRoomSizeRage.x);
-            
+
             room1 = new RectInt(pRoom.x, pRoom.y, splitRandom + 1, pRoom.height);
-            room2 = new RectInt(pRoom.x + splitRandom , pRoom.y, pRoom.width - splitRandom, pRoom.height);
+            room2 = new RectInt(pRoom.x + splitRandom, pRoom.y, pRoom.width - splitRandom, pRoom.height);
         }
-       else//horizontal
-       {
+        else//horizontal
+        {
             splitRandom = Random.Range((int)minRoomSizeRage.x, pRoom.height - (int)minRoomSizeRage.x);
 
             room1 = new RectInt(pRoom.x, pRoom.y, pRoom.width, splitRandom + 1);
-            room2 = new RectInt(pRoom.x, pRoom.y + splitRandom - 1, pRoom.width , pRoom.height - splitRandom + 1);
-           
-       }
+            room2 = new RectInt(pRoom.x, pRoom.y + splitRandom, pRoom.width, pRoom.height - splitRandom);
+
+        }
         //Debug.Log(splitRandom);
-        roomsToSplit.Add(room1); roomsToSplit.Add(room2);
+        roomsToSplit.Insert(0, room1); roomsToSplit.Insert(1, room2);
+    }
+
+    void MakeDoor(RectInt intersect)
+    {
+        RectInt door = new RectInt(0, 0, 0, 0); int positionRandom;
+        if (intersect.width == 1)
+        {
+            positionRandom = Random.Range(2, intersect.height-3);
+            door = new RectInt(intersect.x,intersect.y+ positionRandom, 1, 2);
+        }
+        else if (intersect.height == 1)
+        {
+            positionRandom = Random.Range(2, intersect.width - 3);
+            door = new RectInt(intersect.x+ positionRandom, intersect.y, 2, 1);
+        }
+        else Debug.LogError("Intersection not valid");
+
+        Doors.Add(door);
     }
 }
