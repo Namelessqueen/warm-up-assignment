68ca578873901311aed215538aee8e004b0f6134
Juliette Landman <Juliettelandman@gmail.com>
2025-03-07 14:59:30 +0100

    Started on Graphs

--- Assets/Scripts/Practice/Graph.cs
+++ Assets/Scripts/Practice/Graph.cs
@@ -1,7 +1,6 @@
+using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEngine;
-
+using UnityEngine;  
 public class Graph<T>
 {
     private Dictionary<T, List<T>> adjacencyList;
@@ -9,13 +8,37 @@ public class Graph<T>
     public Graph() { adjacencyList = new Dictionary<T, List<T>>(); }
     public void AddNode(T node)
     {
-
+        if(!adjacencyList.ContainsKey(node)) adjacencyList[node] = new List<T>();
     }
     public void AddEdge(T fromNode, T toNode)
     {
-
+        if (!adjacencyList.ContainsKey(fromNode) || !adjacencyList.ContainsKey(toNode))
+        {
+            Debug.Log("One or both nodes do not exist in the graph.");
+            return;
+        }
+        adjacencyList[fromNode].Add(toNode);
+        adjacencyList[toNode].Add(fromNode);
     }
 
+    public void Print()
+    {
+        foreach (var ele in adjacencyList)
+        {
+            Debug.Log($"Node: {ele.Key}, connections: {ele.Value}");
+            foreach (var item in ele.Value)
+            { Debug.Log($"connections: {item}"); }
+        }
+    }
 
+    public List<T> GetNeighbors(T node)
+    {
 
+        if (!adjacencyList.ContainsKey(node))
+        {
+            Debug.Log("Node does not exist in the graph.");
+        }        
+        return adjacencyList[node];
+    }
+    
 }
---
+++ Assets/Scripts/Practice/GraphTester.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class GraphTester : MonoBehaviour
+{
+    Graph<string> graph = new Graph<string>();
+    void Start()
+    {
+        graph.AddNode("A");
+        graph.AddNode("B");
+        graph.AddNode("C");
+        graph.AddNode("D");
+        graph.AddEdge("A", "B");
+        graph.AddEdge("A", "C");
+        graph.AddEdge("B", "D");
+        graph.AddEdge("C", "D");
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log("Graph Structure:");
+            graph.Print();
+        }
+    }
+}
